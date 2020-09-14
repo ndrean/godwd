137a27a9baf7431a82ea4045236ae733ca8950a9
neven <nevendrean@yahoo.fr>
2020-09-14 05:17:02 +0200

    test CL cred

--- app/workers/remove_direct_link.rb
+++ app/workers/remove_direct_link.rb
@@ -3,7 +3,13 @@ class RemoveDirectLink
   #queue_as :default
 
     def perform(event_publicID)
+      auth = {
+        cloud_name: ENV['CL_CLOUD_NAME'],
+        api_key: ENV['CL_API_KEY'],
+        api_secret: ENV['CL_API_SECRET']
+      }
+      
       return if !event_publicID
-      Cloudinary::Uploader.destroy(event_publicID)
+      Cloudinary::Uploader.destroy(event_publicID, auth)
     end
 end
