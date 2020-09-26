83d98c24cf23754f4bd310950fa6bcd90347e71e
neven <nevendrean@yahoo.fr>
2020-09-26 15:40:10 +0200

    puma setup

--- config/puma.rb
+++ config/puma.rb
@@ -34,7 +34,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # cf HEROKU
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#recommended-default-puma-process-and-thread-configuration
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-# worker 1
+worker 2
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
@@ -48,7 +48,7 @@ rackup DefaultRackup
 
 
 before_fork do 
-    @sidekiq_pid ||= spawn('bundle exec sidekiq -t 25')
+    @sidekiq_pid ||= spawn('bundle exec sidekiq -t 2')
 end
 
 #### NGINX  buildpack ###
