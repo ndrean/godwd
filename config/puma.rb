8f28340b8793155d07f3e973bfc11b9b1f1f38a3
neven <nevendrean@yahoo.fr>
2020-09-28 18:53:28 +0200

    nginx

--- config/puma.rb
+++ config/puma.rb
@@ -52,8 +52,8 @@ rackup DefaultRackup
 # end
 
 #### NGINX  buildpack ###
-# bind ENV.fetch('PUMA_SOCK') { 'unix:///tmp/nginx.socket' }
-listen '/tmp/nginx.socket'
+bind ENV.fetch('PUMA_SOCK') { 'unix:///tmp/nginx.socket' }
+# listen '/tmp/nginx.socket'
 before_fork do |server,worker|
 	FileUtils.touch('/tmp/app-initialized')
 end
