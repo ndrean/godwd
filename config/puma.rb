c06339dc2cffd325e931c00231d4c05193a436aa
neven <nevendrean@yahoo.fr>
2020-10-03 01:26:37 +0200

    nginx

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,8 @@
 
 require 'fileutils'
 
-workers     ENV.fetch('WEB_CONCURRENCY') {1}
+# puma in single mode => set wrokers to 'O'
+workers     ENV.fetch('WEB_CONCURRENCY') {2}
 
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
