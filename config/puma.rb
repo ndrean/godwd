7855dbeb23cd65790630623eae131dacd676f969
neven <nevendrean@yahoo.fr>
2020-10-02 22:20:41 +0200

    nginx

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,3 @@
-# Using setup:
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
 
 require 'fileutils'
@@ -8,7 +7,7 @@ workers     Integer(ENV['WEB_CONCURRENCY'] || 1)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-port  ENV['PORT'] || 3001
+port        ENV['PORT'] || 3001
 
 environment ENV.fetch("RAILS_ENV") { "development" } 
 
