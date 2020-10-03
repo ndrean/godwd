7f7b9cc18bd6ec0ddb74cdeea64e518df566f127
neven <nevendrean@yahoo.fr>
2020-10-03 03:54:59 +0200

    nginx domain

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ workers     ENV.fetch('WEB_CONCURRENCY') {2}
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-port        ENV['PORT']
+port        ENV['PORT'] || 3001
 
 environment ENV.fetch("RAILS_ENV") { "development" } 
 
