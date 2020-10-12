1927687ded82284279e9cc2891ab97f0ad91b07a
neven <nevendrean@yahoo.fr>
2020-10-13 01:44:38 +0200

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["http://localhost:3000/", "http://localhost:8080","https://thedownwinder.com"]
+    origins ["https://localhost:3000", "http://localhost:8080","https://thedownwinder.com"]
 
     resource "*",
       headers: :any,
