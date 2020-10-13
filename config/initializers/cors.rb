574063ac687ac4a91cfe43877404eb9970b14675
neven <nevendrean@yahoo.fr>
2020-10-13 02:08:31 +0200

    cors multiple

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,21 +5,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allowed_headers = %i(get post put patch delete options head)
   allow do
-    origins "*"#, "http://localhost:8080", "https://thedownwinder.com"
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-
-  allow do
-    origins "http://localhost:8080"
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-
-  allow do
-    origins "https://thedownwinder.com"
+    origins '*'
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
